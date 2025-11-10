39c77bd4968e7091c5ce9cb2354393554c64c3cb
karolzinha2008 <ana.karolinede.oliveira@escola.pr.gov.br>
2025-11-10 14:53:38 -0300

    Add files via upload

---
+++ html/script.js
@@ -0,0 +1,8 @@
+// Controla a velocidade do letreiro
+const letreiro = document.querySelector('.letreiro');
+
+// Definindo a velocidade da animação, que é de 12 segundos
+const velocidade = 20;
+
+// Atribui a duração da animação ao letreiro
+letreiro.style.animationDuration = `${velocidade}s`;
